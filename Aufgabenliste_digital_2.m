1e79e03562f4c321adb2909661580345427d08b7
danstadelmann <dasta_7@hotmail.com>
2015-09-21 16:03:03 +0200

    solutions for digitalteil uebung 2 und 3

--- Aufgabenliste_digital_2.m
+++ Aufgabenliste_digital_2.m
@@ -5,3 +5,13 @@ A2=1;
 w1=2*pi/10;
 w2=2*pi/0.1;
 
+sim('Aufgabenliste_digital_2_simulink');
+
+figure(1);
+plot(ys.time,ys.signals.values,...
+yk.time,yk.signals.values,'r');
+
+%G=tf([1],[1/4,1]);
+%bode(G)
+
+
